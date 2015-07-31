d1bba9179037f570ea1973e6b1abe51d3c6d231d
Alan Bradley <alan@gangleri.net>
2015-07-31 23:19:15 +0100

    using sh for the installer

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
-#!/bin/zsh
+#!/bin/sh
 
 set -e 
 
 { # ensure the entire script is downloaded
 
-	local SCRIPT_URL='https://raw.githubusercontent.com/gangleri/toggle-touchpad/master/toggle-touchpad'
-	local INSTALL_NAME='/usr/local/bin/toggle-touchpad'
-	local SHORT_NAME='/usr/local/bin/ttp'
+	SCRIPT_URL='https://raw.githubusercontent.com/gangleri/toggle-touchpad/master/toggle-touchpad'
+	INSTALL_NAME='/usr/local/bin/toggle-touchpad'
+	SHORT_NAME='/usr/local/bin/ttp'
 	
-	curl -s -L -o $INSTALL_NAME $SCRIPT_URL
+	curl  -s -L -o $INSTALL_NAME $SCRIPT_URL
 	chmod +x $INSTALL_NAME
+	rm -f $SHORT_NAME
 	ln -s $INSTALL_NAME $SHORT_NAME
 
 }
